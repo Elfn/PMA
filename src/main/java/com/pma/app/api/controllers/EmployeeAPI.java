57e4beae7cc9c0f322dee0af8413d5ac9d8e921f
Elimane Fofana <Elimane.Fofana@supinfo.com>
2020-05-30 19:29:54 +0000

    Rest API's [IP] #2

---
+++ src/main/java/com/pma/app/api/controllers/EmployeeAPI.java
@@ -0,0 +1,10 @@
+package com.pma.app.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Elimane on May, 2020, at 19:29
+ */
+@RestController
+public class EmployeeAPI {
+}
